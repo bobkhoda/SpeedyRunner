9b3741339ec6ba1b8a29e0bed1f2ad794eb1b980
unknown <bob_deus@yahoo.com>
2015-10-06 22:13:51 -0600

    Finally got the scss correct for the calendar.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module SpeedyRunner
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.time_zone = "Mountain Time (US & Canada)"
+    config.beginning_of_week = :sunday
   end
 end
